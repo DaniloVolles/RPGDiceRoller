4553ec7dd0363e78edf7d429f5364569909a5252
Danilo <danilo.volles@outlook.com>
2023-01-13 21:08:52 -0300

    Background Color update. Button-function implementation.

--- app/src/main/java/com/danilovolles/rpgdiceroller/MainActivity.kt
+++ app/src/main/java/com/danilovolles/rpgdiceroller/MainActivity.kt
@@ -2,6 +2,8 @@ package com.danilovolles.rpgdiceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 val d4 = Dice(4)
 val d6 = Dice(6)
@@ -12,11 +14,63 @@ val d20 = Dice(20)
 val d100 = Dice(100)
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollResult: TextView
+    private lateinit var btnD4Var: Button
+    private lateinit var btnD6Var: Button
+    private lateinit var btnD8Var: Button
+    private lateinit var btnD10Var: Button
+    private lateinit var btnD12Var: Button
+    private lateinit var btnD20Var: Button
+    private lateinit var btnD100Var: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollResult = findViewById(R.id.rollVisualization)
+        btnD4Var = findViewById(R.id.btnD4)
+        btnD6Var = findViewById(R.id.btnD6)
+        btnD8Var = findViewById(R.id.btnD8)
+        btnD10Var = findViewById(R.id.btnD10)
+        btnD12Var = findViewById(R.id.btnD12)
+        btnD20Var = findViewById(R.id.btnD20)
+        btnD100Var = findViewById(R.id.btnD100)
+
     }
 
-    var d4Result = d4.rollDie()
+    fun rollD4Btn() {
+        val result = d4.rollDie()
+        rollResult.text = result.toString()
+    }
 
+    fun rollD6Btn() {
+        val result = d6.rollDie()
+        rollResult.text = result.toString()
+    }
+
+    fun rollD8Btn() {
+        val result = d8.rollDie()
+        rollResult.text = result.toString()
+    }
+
+    fun rollD10Btn() {
+        val result = d10.rollDie()
+        rollResult.text = result.toString()
+    }
+
+    fun rollD12Btn() {
+        val result = d12.rollDie()
+        rollResult.text = result.toString()
+    }
+
+    fun rollD20Btn() {
+        val result = d20.rollDie()
+        rollResult.text = result.toString()
+    }
+
+    fun rollD100Btn() {
+        val result = d100.rollDie()
+        rollResult.text = result.toString()
+    }
 }
